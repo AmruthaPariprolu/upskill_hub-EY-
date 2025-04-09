6bb52ed82309180cf281798530182249086fa4a4
AmruthaPariprolu <pamruthalaxmi@gmail.com>
2025-04-09 23:26:29 +0530

    add

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcryptjs");
+
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
